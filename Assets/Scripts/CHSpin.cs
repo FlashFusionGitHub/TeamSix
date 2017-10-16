cbb960f2713773ff7745797f4ded441b80ad7df1
AIE\s165034 <brodie.hwite@gmail.com>
2017-10-16 16:04:07 +1030

    fixed commit, files in wrong spot

---
+++ Assets/Scripts/CHSpin.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CHSpin : MonoBehaviour {
+    public float spinSpeed = 1;
+
+    private CharacterController controller;
+	// Use this for initialization
+	void Start () {
+        controller = gameObject.GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float spin = spinSpeed * Time.deltaTime;
+        transform.Rotate(0, 0, spin);
+	}
+}
