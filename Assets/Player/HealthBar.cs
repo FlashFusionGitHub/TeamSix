f930fd48641986008608b309f86ed34a7c894968
Rowen <rowen.govender93@gmail.com>
2017-10-23 16:03:54 +1030

    Added Git Ignore File

---
+++ Assets/Player/HealthBar.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public Image currentHealthbar;
+    public Text ratioText;
+
+    private float health = 100;
+    private float max_health = 100;
+
+    float timer = 3;
+    void OnCollisionEnter(Collision hit)
+    {
+        if (hit.collider.tag == "Enemy")
+        {
+            TakeDamge(10);
+        }
+
+        if(hit.collider.tag == "Health")
+        {
+            HealDamage(10);
+        }
+    }
+
+    void UpdateHealthBar()
+    {
+        float ratio = health / max_health;
+
+        currentHealthbar.rectTransform.localScale = new Vector3(ratio, 1, 1);
+        ratioText.text = (ratio * 100).ToString() + '%';
+    }
+
+    private void TakeDamge(float damage)
+    {
+        health -= damage;
+
+        if (health < 0)
+            health = 100; // set health back to 100 if the player dies
+
+        UpdateHealthBar();
+    }
+
+    private void HealDamage(float heal)
+    {
+        health += heal;
+
+        if (health > 100)
+            health = max_health;
+
+        UpdateHealthBar();
+    }
+}
---
+++ Assets/Player/ItemPickUp.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemPickUp : MonoBehaviour
+{
+    public Text KeysCollectedText;
+    public Text AmmoText;
+    public Text PickUpText;
+    private int numberOfKeys = 1;
+    private int ammoCount;
+    private int maxAmmo = 30;
+    bool Destroyed;
+
+    void Start()
+    {
+        KeysCollectedText.text = "Keys Collected: 0";
+        AmmoText.text = "Ammo: " + ammoCount;
+        Destroyed = false;
+    }
+
+    void Update()
+    {
+        if(Destroyed == true)
+        {
+            numberOfKeys += 1;
+            Destroyed = false;
+        }
+    }
+
+    void OnTriggerStay(Collider hit)
+    {
+        if (hit.tag == "Key")
+        {
+            PickUpText.text = "Press 'E' to collect key";
+            if (Input.GetKey(KeyCode.E))
+            {
+                Destroy(hit.gameObject);
+                Destroyed = true;
+                KeysCollectedText.text = "Keys Collected: " + numberOfKeys;
+                PickUpText.text = "";
+            }
+        }
+
+        if(hit.tag == "Ammo")
+        {
+            if (ammoCount < maxAmmo)
+            {
+                ammoCount += 6;
+                AmmoText.text = "Ammo: " + ammoCount;
+                Destroy(hit.gameObject);
+            }
+            else
+                return;
+        }
+    }
+
+    void OnTriggerExit(Collider hit)
+    {
+        PickUpText.text = "";
+    }
+}
