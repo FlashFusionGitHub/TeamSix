e2d36245c25e522255c22619afa1788c28823d68
Rowen <rowen.govender93@gmail.com>
2017-10-24 23:00:58 +1030

    Added player Controller

--- Assets/Scripts/CHSpin.cs
+++ Assets/Scripts/CHSpin.cs
@@ -3,17 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CHSpin : MonoBehaviour {
-    public float spinSpeed = 1;
 
-    private CharacterController controller;
+    public float spin_speed;
+
 	// Use this for initialization
 	void Start () {
-        controller = gameObject.GetComponent<CharacterController>();
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        float spin = spinSpeed * Time.deltaTime;
-        transform.Rotate(0, 0, spin);
+
+        transform.Rotate(0, 0, spin_speed * Time.deltaTime);
 	}
 }
---
+++ Assets/Scripts/ItemPickUp.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemPickUp : MonoBehaviour
+{
+    public Text KeysCollectedText;
+    public Text AmmoText;
+    public Text PickUpText;
+    private int numberOfKeys = 1;
+    private int ammoCount;
+    private int maxAmmo = 30;
+    bool Destroyed;
+
+    void Start()
+    {
+        KeysCollectedText.text = "Keys Collected: 0";
+        AmmoText.text = "Ammo: " + ammoCount;
+        Destroyed = false;
+    }
+
+    void Update()
+    {
+        if (Destroyed == true)
+        {
+            numberOfKeys += 1;
+            Destroyed = false;
+        }
+
+        Shoot();
+    }
+
+
+    void Shoot()
+    {
+        if(Input.GetMouseButtonDown(0) && ammoCount > 0)
+        {
+            ammoCount -= 1;
+            AmmoText.text = "Ammo: " + ammoCount;
+        }
+        return;
+    }
+
+    void OnTriggerStay(Collider hit)
+    {
+        if (hit.tag == "Key")
+        {
+            PickUpText.text = "Press 'E' to collect key";
+            if (Input.GetKey(KeyCode.E))
+            {
+                Destroy(hit.gameObject);
+                Destroyed = true;
+                KeysCollectedText.text = "Keys Collected: " + numberOfKeys;
+                PickUpText.text = "";
+            }
+        }
+    }
+
+    void OnTriggerEnter(Collider hit)
+    {
+        if (hit.tag == "Ammo")
+        {
+            if (ammoCount < maxAmmo)
+            {
+                ammoCount += 6;
+
+                if (ammoCount > maxAmmo)
+                    ammoCount = maxAmmo;
+
+                AmmoText.text = "Ammo: " + ammoCount;
+                Destroy(hit.gameObject);
+            }
+            else
+                return;
+        }
+    }
+
+    void OnTriggerExit(Collider hit)
+    {
+        PickUpText.text = "";
+    }
+}
